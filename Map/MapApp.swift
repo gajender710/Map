da185e99735d9360863015895e3cb6a1c3c839d9
gajender <gajenderj@orientaloutsourcing.com>
2022-05-12 12:35:53 +0530

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Gajender Jangir on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
